44a4148399a9b84dd495027a6c973208d57cd6a4
donghhan <apensia914@gmail.com>
2023-11-29 11:55:28 +0700

    Rollback for RENDER deploy

--- config/settings.py
+++ config/settings.py
@@ -6,17 +6,18 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = "RENDER" not in os.environ
 
 SECRET_KEY = os.environ.get(
-    "SECRET_KEY", "django-insecure-naYx2i1YAC3Poaz340PDdOBZBxxI3nxH"
+    "SECRET_KEY", default="django-insecure-naYx2i1YAC3Poaz340PDdOBZBxxI3nxH"
 )
 
 # Allowed Host
-if not DEBUG:
-    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-else:
-    ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 BUILTIN_APPS = [
@@ -65,13 +66,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Middleware
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 # Database
@@ -150,8 +151,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
