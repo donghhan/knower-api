3a36634f51653cec347c29531d65263324bb6f50
donghhan <apensia914@gmail.com>
2023-11-29 11:42:47 +0700

    Deleted DEBUG condition for whitenoise

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", True)
 
 SECRET_KEY = os.environ.get(
     "SECRET_KEY", "django-insecure-naYx2i1YAC3Poaz340PDdOBZBxxI3nxH"
@@ -150,10 +150,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
